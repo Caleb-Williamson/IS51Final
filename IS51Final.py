25198fac7452d22ed67d1b717b5204327d77ae2e
Caleb-Williamson <calebdogboss@mail.fresnostate.edu>
2022-05-19 16:13:38 -0700

    pseudo-code

--- IS51Final.py
+++ IS51Final.py
@@ -8,3 +8,43 @@ Lastly implement a loop that checks to see if each score is higher than the avg 
 Then determine the percentage of scores that are above the average and display.
 
 """
+"""
+#calculate_percent_above_average function
+def calculate_percent_above_average(ll,avg):
+        cnt=0
+        for a in ll:
+                if(a>avg):
+                        cnt+=1
+        return ((cnt/len(ll))*100)
+
+#main function
+
+if __name__ == "__main__":
+        opening python file
+        f = open("Final.txt", "r")
+        #spliting element by enter
+        l=f.read().split("\n")
+        ll = []
+        for a in l:
+                if(a!=''):
+                        ll.append(int(a))
+        #printing number of grades
+        print("Number of grades : ",len(ll))
+
+        calculate average grade
+        avg=0
+        for a in ll:
+                avg+=a
+        avg=avg/len(ll)
+        print("Average of grades : ",avg)
+        print("Percentage of grades above average: ",calculate_percent_above_average(ll,avg),"%")
+
+
+
+
+
+"""
+
+
+
+
