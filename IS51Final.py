6ec6b08ba79973e319b6765c49cfe76eb5e86a5d
Caleb-Williamson <calebdogboss@mail.fresnostate.edu>
2022-05-19 16:05:43 -0700

    structured-english

---
+++ IS51Final.py
@@ -0,0 +1,10 @@
+"""
+This program calculates the number or grades, the average, and percentage of grades abouve average.
+
+It does this by opening the file Final.txt and saving the scores in memory, then close the file. 
+Next count the scores, print result.
+Then, summ the scores and divide by the above count.
+Lastly implement a loop that checks to see if each score is higher than the avg score. 
+Then determine the percentage of scores that are above the average and display.
+
+"""
